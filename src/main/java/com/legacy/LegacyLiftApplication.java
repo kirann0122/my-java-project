49585fa38d4ea4809e991ee2d43eaeb7e49e329a
kirann0122 <kirangvkirangv60@gmail.com>
2026-08-13 12:03:17 +0530

    feat: Integrate the backend and frontend components

--- src/main/java/com/legacy/LegacyLiftApplication.java
+++ src/main/java/com/legacy/LegacyLiftApplication.java
@@ -2,11 +2,19 @@ package com.legacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LegacyLiftApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LegacyLiftApplication.class, args);
+    }
+
+    @GetMapping("/api/legacyCodebase")
+    public String getLegacyCodebase() {
+        return "Legacy Codebase";
     }
 }
--- src/main/java/com/legacy/controller/LegacyCodebaseController.java
+++ src/main/java/com/legacy/controller/LegacyCodebaseController.java
@@ -2,18 +2,30 @@ package com.legacy.controller;
 
 import com.legacy.analysis.JavaParserAnalyzer;
 import com.legacy.analysis.RiskGraph;
+import com.legacy.legacyCodebase.LegacyCodebase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
+
 @RestController
 public class LegacyCodebaseController {
-    private JavaParserAnalyzer javaParserAnalyzer;
 
-    public LegacyCodebaseController() {
-        this.javaParserAnalyzer = new JavaParserAnalyzer();
+    private final JavaParserAnalyzer javaParserAnalyzer;
+
+    @Autowired
+    public LegacyCodebaseController(JavaParserAnalyzer javaParserAnalyzer) {
+        this.javaParserAnalyzer = javaParserAnalyzer;
     }
 
-    @GetMapping("/risk-graph")
+    @GetMapping("/api/analyzeLegacyCodebase")
+    public String analyzeLegacyCodebase() throws FileNotFoundException {
+        javaParserAnalyzer.analyze("src/main/java");
+        return "Legacy Codebase analyzed";
+    }
+
+    @GetMapping("/api/getRiskGraph")
     public RiskGraph getRiskGraph() {
         return javaParserAnalyzer.getRiskGraph();
     }
